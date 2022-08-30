d85031c448291ede6bf695d8ed1d112a3bc40f37
sarojsubedi <sarojsubedi@localhost>
2022-08-30 18:57:17 -0400

    This is my commit for assignment 1.

---
+++ lsp-subedi/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * Sanjaya Subedi
+ * 
+ *ghp_zAB23fAJiof85wfO1ITWernWH50iuN4T27s7 
+ *  
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!");
+
+	}
+
+}
