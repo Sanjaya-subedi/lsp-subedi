4f845438d29824d68ac03c94e55f27f6b56e10a2
Sanjaya-subedi <78467026+Sanjaya-subedi@users.noreply.github.com>
2022-11-23 16:51:09 -0500

    Create EmptyListException.java

---
+++ lsp-subedi/src/org/howard/edu/lsp/assignment6/EmptyListException.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment6;
+
+/**
+ * {@summary to handle the empty list}
+ * @author Sanjaya Subedi
+ *
+ */
+public class EmptyListException extends Exception {
+	
+	/**
+	 * @return void
+	 * {@summary to show the error message}
+	 */
+	public EmptyListException() {
+		super("Empty list cannot have values to calculate averages");
+	}
+}
